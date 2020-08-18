f174e19ce032f2f31b6d44a8dc14887b17d50504
Yasin Tanriverdi <yasintanriverdi@hotmail.com>
2020-08-18 07:28:37 +0200

    Initial project setup

---
+++ src/test/kotlin/tacos/TacoCloudApplicationTests.kt
@@ -0,0 +1,25 @@
+package tacos
+
+import org.hamcrest.Matchers.containsString
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@WebMvcTest(HomeController::class)
+class TacoCloudApplicationTests {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun testHomePage() {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk)
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to...")))
+    }
+
+}
